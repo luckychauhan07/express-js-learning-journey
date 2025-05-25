ba0b6d941e625fc34cf4c13c1c3fc766b92df09b
luckychauhan07 <chauhan12lucky@gmail.com>
2025-05-25 22:35:12 +0530

    ejs completed

--- assignment1 ejs/routes/hostRouter.js
+++ assignment1 ejs/routes/hostRouter.js
@@ -9,15 +9,19 @@ const path = require("path");
 
 const hostRouter = express.Router();
 hostRouter.get("/contactUs", (req, res, next) => {
-    res.sendFile(path.join(rootpath, "views", "homeRegister.html"));
+    res.render("homeRegister");
 });
 const registerHomes = [];
 hostRouter.post("/contactUs", (req, res, next) => {
     console.log(req.body, req.body.houseName);
     registerHomes.push({
         houseName: req.body.houseName,
+        charge: req.body.chargePerNight,
+        location: req.body.location,
+        rating: req.body.rating,
+        imageUrl: req.body.imageUrl,
     });
-    res.sendFile(path.join(rootpath, "views", "success.html"));
+    res.render("success");
 });
 exports.hostRouter = hostRouter;
 exports.registerHomes = registerHomes;
